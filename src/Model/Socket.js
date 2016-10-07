8b7dc7352b51efb3485f9956bf16b91409267927
eadon80 <eadon80@gmail.com>
2016-10-07 20:50:13 +0300

    fix deploy

--- src/Model/Socket.js
+++ src/Model/Socket.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 
 export class Socket {
     constructor(matrix, game, config) {
-        this.socket = io('http://localhost:5000');
+        this.socket = io('https://' + window.location.hostname);
         this.matrix = matrix;
         this.game = game;
         this.config = config;
